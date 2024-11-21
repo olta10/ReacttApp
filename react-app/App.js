40893790cb3033f7c2f821a3c1741cb5ae5a0093
olta10 <olta.kadrolli122@gmail.com>
2024-11-22 00:18:24 +0100

    react1

--- react-app/App.js
+++ react-app/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ProductScreen from './ProductScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <ProductScreen></ProductScreen>
     </View>
   );
 }
